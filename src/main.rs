2a16a8c4ad74c783826c9c196c17a0d3eb1fdd78
Christopher Karani <chrisbkarani@gmail.com>
2020-01-27 01:07:31 +0300

    made some changes

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,11 @@
 extern crate reqwest;
 extern crate serde_json;
+mod pokeapi_endpoint;
 
-use std::borrow::Borrow;
 
-static BASE_URL: &str = "https://pokeapi.co/api/v2";
 
 fn main() {
     println!("Hello World");
     let endpoint = PokeAPIEndpoint::build();
     endpoint.get_pokemon("ditto")
-}
-
-/// Use this to build endpoints from the PokeApi
-struct PokeAPIEndpoint {
-    path: String,
-}
-
-impl PokeAPIEndpoint {
-    /// create an instance of the endpoint builder
-    /// uses the base url: `"https://pokeapi.co/api/v2"`
-    fn build() -> PokeAPIEndpoint {
-        PokeAPIEndpoint { path: String::from(BASE_URL), }
-    }
-    /// Use this to create an endpoint from the
-    /// # Arguments
-    /// * `endpoint` - the path of the API you want to access using the base URL of
-    ///  "`https://pokeapi.co/api/v2"`
-    fn make_pokeapi_endpoint(&self, endpoint: &str) -> PokeAPIEndpoint {
-        let url_string = self.path.clone();
-        let new_string = format!("{}{}", url_string, endpoint);
-        PokeAPIEndpoint { path: new_string }
-    }
-    /// construct an endpoint with a "/pokemon" path simply give the name of the pokemon
-    /// to begin the query
-    /// # Arguments
-    /// *`pokemon_name`- the name of the pokemon you want to look up
-    ///
-    fn construct_pokemon_enpoint(&self, pokemon_name: &str) -> PokeAPIEndpoint {
-        let path = String::from("/pokemon");
-        let endpoint = format!("{}/{}", path, pokemon_name);
-        self.make_pokeapi_endpoint(endpoint.as_str())
-    }
-
-    fn get_pokemon(&self,name: &str) {
-        let endpoint = self.construct_pokemon_enpoint(name);
-        let endpoint_path = endpoint.path;
-
-        let response = reqwest::get(endpoint_path).await;
-        println!("getting pokemon");
-        println!("Endpoint: {}", endpoint_path);
-    }
 }
---
+++ src/pokeapi_endpoint.rs
@@ -0,0 +1,43 @@
+
+static BASE_URL: &str = "https://pokeapi.co/api/v2";
+
+/// Use this to build endpoints from the PokeApi
+pub struct PokeAPIEndpoint {
+    path: String,
+}
+
+impl PokeAPIEndpoint {
+    /// create an instance of the endpoint builder
+    /// uses the base url: `"https://pokeapi.co/api/v2"`
+    pub fn build() -> PokeAPIEndpoint {
+        PokeAPIEndpoint { path: String::from(BASE_URL), }
+    }
+    /// Use this to create an endpoint from the
+    /// # Arguments
+    /// * `endpoint` - the path of the API you want to access using the base URL of
+    ///  "`https://pokeapi.co/api/v2"`
+    fn make_pokeapi_endpoint(&self, endpoint: &str) -> PokeAPIEndpoint {
+        let url_string = self.path.clone();
+        let new_string = format!("{}{}", url_string, endpoint);
+        PokeAPIEndpoint { path: new_string }
+    }
+    /// construct an endpoint with a "/pokemon" path simply give the name of the pokemon
+    /// to begin the query
+    /// # Arguments
+    /// *`pokemon_name`- the name of the pokemon you want to look up
+    ///
+    fn construct_pokemon_enpoint(&self, pokemon_name: &str) -> PokeAPIEndpoint {
+        let path = String::from("/pokemon");
+        let endpoint = format!("{}/{}", path, pokemon_name);
+        self.make_pokeapi_endpoint(endpoint.as_str())
+    }
+
+    pub fn get_pokemon(&self, name: &str) {
+        let endpoint = self.construct_pokemon_enpoint(name);
+        let endpoint_path = endpoint.path;
+
+//        let response = reqwest::get(endpoint_path).await;
+        println!("getting pokemon");
+        println!("Endpoint: {}", endpoint_path);
+    }
+}
